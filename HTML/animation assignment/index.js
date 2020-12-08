a34fdcef9c5455a308bab723d8676c8d8a2fce03
Nauman1971 <namanaligoku@gmail.com>
2020-12-08 23:38:00 +0500

    all code

---
+++ HTML/animation assignment/index.js
@@ -0,0 +1,5 @@
+setTimeout(() => {
+    document.getElementById("first-wheel").classList.add('wheel-moving');
+    document.getElementById("second-wheel").classList.add('wheel-moving');
+    document.getElementById("container-id").classList.add('container-moving');
+    }, 1000)
